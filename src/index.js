d5b4b2a67bc50db5be885c5d64d88289b5d524cc
Carlton Joseph <carlton.joseph@gmail.com>
2020-09-03 14:33:45 -0700

    Reduce boilerplate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import "./styles.css";
@@ -19,39 +19,28 @@ const SignupForm = () => {
         email: Yup.string().email("Invalid email address").required("Required"),
       })}
       onSubmit={(values, { setSubmitting }) => {
-        alert(JSON.stringify(values, null, 2));
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
       }}
     >
       {(formik) => (
-        <form onSubmit={formik.handleSubmit}>
+        <Form>
           <label htmlFor="firstName">First Name</label>
-          <input
-            id="firstName"
-            type="text"
-            {...formik.getFieldProps("firstName")}
-          />
-          {formik.touched.firstName && formik.errors.firstName ? (
-            <div>{formik.errors.firstName}</div>
-          ) : null}
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
 
           <label htmlFor="lastName">Last Name</label>
-          <input
-            id="lastName"
-            type="text"
-            {...formik.getFieldProps("lastName")}
-          />
-          {formik.touched.lastName && formik.errors.lastName ? (
-            <div>{formik.errors.lastName}</div>
-          ) : null}
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
 
           <label htmlFor="email">Email Address</label>
-          <input id="email" type="email" {...formik.getFieldProps("email")} />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
 
           <button type="submit">Submit</button>
-        </form>
+        </Form>
       )}
     </Formik>
   );
